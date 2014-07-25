96c406073fc938e1d25a578073ce15b6fcf2d94c
Koeun Kim <kimkoen@naver.com>
2014-07-25 15:34:28 +0900

    최초 등록

---
+++ app/src/main/java/kke/travelplan/IntroActivity.java
@@ -0,0 +1,48 @@
+package kke.travelplan;
+
+import kke.travelplan.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import kke.travelplan.R;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class IntroActivity extends Activity {
+    private Runnable irun = new Runnable() {
+        public void run() {
+            Intent i = new Intent(IntroActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_intro);
+
+        final View controlsView = findViewById(R.id.fullscreen_content_controls);
+        final View contentView = findViewById(R.id.fullscreen_content);
+
+        Handler h = new Handler();
+        h.postDelayed(irun, 4000);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+
+}
