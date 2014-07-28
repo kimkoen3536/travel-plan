8ac470b2da409ae1ba730de877c0ff2f7607bc18
KoEunKim <kimkoen@naver.com>
2014-07-29 00:42:21 +0900

    비밀번호 찾기, 회원가입 화면 구성

---
+++ app/src/main/java/kke/travelplan/SignUpFormActivity.java
@@ -0,0 +1,45 @@
+package kke.travelplan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import kke.travelplan.R;
+
+public class SignUpFormActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_form);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.sign_up_form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void confirmButtonOnClick(View view) {
+        Intent i = new Intent(this, SingUpResultActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
