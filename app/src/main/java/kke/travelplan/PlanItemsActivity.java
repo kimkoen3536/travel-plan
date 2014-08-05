ba6bda3924744937bd7e1ac6b3b6079530eda4b0
Koeun Kim <kimkoen@naver.com>
2014-08-05 19:02:52 +0900

    컨텍스트 메뉴 꼼수

--- app/src/main/java/kke/travelplan/PlanItemsActivity.java
+++ app/src/main/java/kke/travelplan/PlanItemsActivity.java
@@ -2,9 +2,12 @@ package kke.travelplan;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -12,6 +15,8 @@ public class PlanItemsActivity extends Activity {
     private Spinner dateSpinner;
 
     private ListView planItemListView;
+
+    private Button contextMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +34,19 @@ public class PlanItemsActivity extends Activity {
         planItemListView = (ListView) findViewById(R.id.plan_item_list_view);
 
         PlanItemListAdapter itemAdapter = new PlanItemListAdapter(this);
-        itemAdapter.add(new PlanItem("한라산","제주도서귀포시","관광") );
-        itemAdapter.add(new PlanItem("까페베네","계룡엄사면","음식"));
+        itemAdapter.add(new PlanItem("한라산", "제주도서귀포시", "관광"));
+        itemAdapter.add(new PlanItem("까페베네", "계룡엄사면", "음식"));
         planItemListView.setAdapter(itemAdapter);
 
+        contextMenuButton = (Button) findViewById(R.id.context_menu_button);
+        registerForContextMenu(contextMenuButton);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.plan_item_list, menu);
+        getMenuInflater().inflate(R.menu.plan_items, menu);
         return true;
     }
 
@@ -53,5 +60,34 @@ public class PlanItemsActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.getId() == R.id.context_menu_button) {
+            menu.add(0, 1, 0, "목적지 추가");
+            menu.add(0, 2, 0, "이동수단 추가");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                return true;
+            case 2:
+                return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                openContextMenu(contextMenuButton);
+                return true;
+        }
+        return true;
     }
 }
