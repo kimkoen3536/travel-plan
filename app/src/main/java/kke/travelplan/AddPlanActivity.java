c3aa8d2b8b9040efa72c218a492f2ea58889a8a7
Koeun Kim <kimkoen@naver.com>
2014-08-05 14:17:38 +0900

    계획추가 화면 구성완료 2

--- app/src/main/java/kke/travelplan/AddPlanActivity.java
+++ app/src/main/java/kke/travelplan/AddPlanActivity.java
@@ -9,18 +9,15 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
-import kke.travelplan.R;
 
 public class AddPlanActivity extends Activity {
 
     private static final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
     private TextView startDateTextView;
     private TextView endDateTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,13 +45,14 @@ public class AddPlanActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-    public void startDateButtonOnClick(View view){
+
+    public void startDateButtonOnClick(View view) {
         DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 Calendar cal = Calendar.getInstance();
                 cal.clear();
-                cal.set(year,monthOfYear,dayOfMonth);
+                cal.set(year, monthOfYear, dayOfMonth);
                 startDateTextView.setText(df.format(cal.getTime()));
 
             }
@@ -67,10 +65,24 @@ public class AddPlanActivity extends Activity {
 
 
     }
-    public void endDateButtonOnClick(View view){
+
+    public void endDateButtonOnClick(View view) {
+        DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar cal = Calendar.getInstance();
+                cal.clear();
+                cal.set(year, monthOfYear, dayOfMonth);
+                endDateTextView.setText(df.format(cal.getTime()));
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        new DatePickerDialog(this, listener, cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH), cal.get(Calendar.DATE)).show();
 
     }
-    public void saveButtonOnClick(View view){
+
+    public void saveButtonOnClick(View view) {
         finish();
     }
 }
