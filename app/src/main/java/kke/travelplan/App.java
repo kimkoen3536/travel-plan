25611e62099fb9f44c32eaf876be4bee4779fef4
KoEunKim <kimkoen@naver.com>
2014-08-22 13:25:13 +0900

    설정 파일 시스템을 Typesafe Config 라이브러리로 교체

--- app/src/main/java/kke/travelplan/App.java
+++ app/src/main/java/kke/travelplan/App.java
@@ -2,12 +2,8 @@ package kke.travelplan;
 
 import android.app.Application;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.util.Properties;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 public class App extends Application {
     public static String urlPrefix;
@@ -15,28 +11,22 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        try {
-            InputStream in = this.getClassLoader().getResourceAsStream("config.properties");
-            Reader reader = new InputStreamReader(in, "UTF-8");
-            Properties props = new Properties();
-            props.load(reader);
-            String host = props.getProperty("kke.travelplan.host");
-            int port = Integer.parseInt(props.getProperty("kke.travelplan.port"));
-            String contextPath = props.getProperty("kke.travelplan.context_path");
-            StringBuilder sb = new StringBuilder();
-            sb.append("http://");
-            sb.append(host);
-            if (port != 80) {
-                sb.append(":");
-                sb.append(port);
-            }
-            sb.append("/");
-            sb.append(contextPath.replaceAll("^\\/|\\/$", ""));
-            urlPrefix = sb.toString();
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("UTF-8을 지원하지 않는다는 에러", e);
-        } catch (IOException e) {
-            throw new RuntimeException("config.properties 읽다가 IO 에러 남", e);
+        Config config = ConfigFactory.load();
+        String host = config.getString("kke.travelplan.host");
+        System.out.println("host : " + host);
+        int port = config.getInt("kke.travelplan.port");
+        System.out.println("port : " + port);
+        String contextPath = config.getString("kke.travelplan.context_path");
+        System.out.println("contextPath : " + contextPath);
+        StringBuilder sb = new StringBuilder();
+        sb.append("http://");
+        sb.append(host);
+        if (port != 80) {
+            sb.append(":");
+            sb.append(port);
         }
+        sb.append("/");
+        sb.append(contextPath.replaceAll("^\\/|\\/$", ""));
+        urlPrefix = sb.toString();
     }
 }
