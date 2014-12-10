3bba6eee80b84f7f4341a324f8f6d9774e677eab
KoEunKim <kimkoen@naver.com>
2014-12-11 01:51:00 +0900

    map 마커찍기 (진행중)

--- app/src/main/java/kke/travelplan/PlaceMapActivity.java
+++ app/src/main/java/kke/travelplan/PlaceMapActivity.java
@@ -1,27 +1,41 @@
 package kke.travelplan;
 
 import android.app.Activity;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapOverlay;
+import com.nhn.android.maps.NMapOverlayItem;
 import com.nhn.android.maps.NMapView;
 import com.nhn.android.maps.NMapView.OnMapStateChangeListener;
 import com.nhn.android.maps.NMapView.OnMapViewTouchEventListener;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.maps.overlay.NMapPOIitem;
+import com.nhn.android.mapviewer.overlay.NMapCalloutOverlay;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager.OnCalloutOverlayListener;
 import com.nhn.android.maps.maplib.NGeoPoint;
 import com.nhn.android.maps.nmapmodel.NMapError;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay.OnStateChangeListener;
 
 
-public class PlaceMapActivity extends NMapActivity implements NMapView.OnMapStateChangeListener,NMapView.OnMapViewTouchEventListener {
+public class PlaceMapActivity extends NMapActivity implements OnMapStateChangeListener,OnMapViewTouchEventListener,OnCalloutOverlayListener {
 
     public static final String API_KEY="f6a3d3afbeaa8b2ca66e9573dae65d2a";
     NMapView mMapView = null;
     NMapController mMapController = null;
     LinearLayout MapContainer;
+    NMapViewerResourceProvider mMapViewerResourceProvider = null;
+    NMapOverlayManager mOverlayManager;
+    OnStateChangeListener onPOIdataStateChangeListener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +49,32 @@ public class PlaceMapActivity extends NMapActivity implements NMapView.OnMapStat
         mMapView.setOnMapViewTouchEventListener(this);
         mMapView.setBuiltInZoomControls(true, null);
         mMapController=mMapView.getMapController();
+        mMapViewerResourceProvider = new NMapViewerResourceProvider(this);
+        mOverlayManager = new NMapOverlayManager(this, mMapView, mMapViewerResourceProvider);
+
+        int markerID = NMapPOIflagType.PIN;
+
+        NMapPOIdata poiData = new NMapPOIdata(2,mMapViewerResourceProvider);
+
+        poiData.addPOIitem(127.0630205,37.5091300, "Pizza 777-111",markerID,0);
+        poiData.addPOIitem(127.061,37.51,"pizza 123-345",markerID,0);
+        poiData.endPOIdata();
+
+        NMapPOIdataOverlay poiDataOverlay = mOverlayManager.createPOIdataOverlay(poiData,null);
+
+        poiDataOverlay.showAllPOIdata(0);
+
+        poiDataOverlay.setOnStateChangeListener(onPOIdataStateChangeListener);
+      //  mOverlayManager.setOnCalloutOverlayListener((OnCalloutOverlayListener)this);
 
 
      //   setContentView(R.layout.activity_place_map);
     }
 
+
+ //   public void onCalloutClick(NMapPOIdataOverlay poiDataOverlay,NMapPOIitem item){
+   //     Toast.makeText(PlaceMapActivity.this, "onCalloutClick: " + item.getTitle(), Toast.LENGTH_LONG).show();
+    //}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,12 +97,12 @@ public class PlaceMapActivity extends NMapActivity implements NMapView.OnMapStat
 
     @Override
     public void onMapInitHandler(NMapView mapview, NMapError errorInfo) {
-        if(errorInfo == null) {
-            mMapController.setMapCenter(new NGeoPoint(126.9738371, 37.5666091), 11);
-        }
-        else {
-                android.util.Log.e("NMap", "onMapInitHandler: error = " + errorInfo.toString());
-            }
+       // if(errorInfo == null) {
+        //    mMapController.setMapCenter(new NGeoPoint(126.9738371, 37.5666091), 11);
+       // }
+       // else {
+    //            android.util.Log.e("NMap", "onMapInitHandler: error = " + errorInfo.toString());
+         //   }
         }
 
 
@@ -120,5 +155,10 @@ public class PlaceMapActivity extends NMapActivity implements NMapView.OnMapStat
     @Override
     public void onSingleTapUp(NMapView mapView, MotionEvent motionEvent) {
 
+    }
+
+    @Override
+    public NMapCalloutOverlay onCreateCalloutOverlay(NMapOverlay nMapOverlay, NMapOverlayItem overlayItem, Rect rect) {
+        return new NMapCalloutBasicOverlay(nMapOverlay, overlayItem, rect);
     }
 }
